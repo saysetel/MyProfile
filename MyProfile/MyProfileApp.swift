4db6cc0d370834fd41d10bca79dc006df69ea7e6
saysetel <saysetel@gmail.com>
2023-02-21 14:20:56 +0100

    Initial Commit

---
+++ MyProfile/MyProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfileApp.swift
+//  MyProfile
+//
+//  Created by Anastasia Kotova on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MyProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
